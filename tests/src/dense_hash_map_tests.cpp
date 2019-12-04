7693990148e208d49266cd27f37511875f4b83b3
Jean Guegant <jguegant@gmail.com>
2019-12-04 22:05:02 +0100

    Started to use a real test.

--- tests/src/dense_hash_map_tests.cpp
+++ tests/src/dense_hash_map_tests.cpp
@@ -1,14 +1,13 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+
 #include "catch2/catch.hpp"
 #include "jg/dense_hash_map.hpp"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
+#include <string>
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE("dense_hash_map", "[factorial]")
+{
+    jg::dense_hash_map<std::string, int> m;
+
+    SECTION("test") { m.emplace("test", 42); }
 }
